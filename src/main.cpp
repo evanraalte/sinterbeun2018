74798cd63f4329b5f042815dc96a3f734933d4d4
Erik van Raalte <e.vanraalte@student.utwente.nl>
2018-11-25 14:07:33 +0100

    Improved interrupt handling and added logic to key pressing

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@
 
 const size_t NUM_SENSORS = 12;
 CapacitiveSensor cs_2[NUM_SENSORS] = {
-  CapacitiveSensor(4,2),
-  CapacitiveSensor(4,3),
-  CapacitiveSensor(4,2),
+  CapacitiveSensor(4,5),
+  CapacitiveSensor(4,6),
+  CapacitiveSensor(4,7),
   CapacitiveSensor(4,3),
   CapacitiveSensor(4,2),
   CapacitiveSensor(4,3),
@@ -22,6 +22,8 @@ CapacitiveSensor cs_2[NUM_SENSORS] = {
   CapacitiveSensor(4,2),
   CapacitiveSensor(4,3),
 };
+
+#define PRESS_THRESHOLD 20
 
 // #define SHORTEST_NOTE 2000
 
@@ -34,6 +36,11 @@ volatile uint8_t overflowCnt = 0;
 volatile uint8_t val = 0;
 volatile uint16_t i = 0;
 
+volatile bool play[NUM_SENSORS] = {false};
+volatile uint16_t playCnt[NUM_SENSORS] = {0}; 
+
+const uint8_t* adresses[] = {c1_wav,c1s_wav,d1_wav,d1s_wav,e1_wav,f1_wav,f1s_wav,g1_wav,g1s_wav,a1_wav,a1s_wav,b1_wav}; //check if this works..
+
 SIGNAL(TIMER2_OVF_vect){
   overflowCnt++;
   if(overflowCnt >= 8){
@@ -41,22 +48,6 @@ SIGNAL(TIMER2_OVF_vect){
     overflowCnt = 0;
     // digitalWrite(LED_BUILTIN, HIGH);s
   }
-
-  if (overflow){
-
-    OCR2A = pgm_read_byte_near(a1_wav + i);
-    if(i == a1_wav_len - 1){
-      i = 0;
-      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    } 
-    else i++;
-
-
-    overflow = false;
-    // digitalWrite(LED_BUILTIN, LOW); 
-  }
-
-
 }
 
 void setup() {
@@ -80,14 +71,64 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  long keys[12] = {  0 }; 
-  for(uint8_t i = 0; i < 12; i++ ){
-    keys[i] = cs_2[i].capacitiveSensor(1);
-    // if(keys[i] > 100) digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    Serial.print(keys[i]);
-    Serial.print('\t');
+  long keys[NUM_SENSORS] = {  0 }; 
+  static bool pressed[NUM_SENSORS] = { false};
+  static bool pressed_old[NUM_SENSORS] = { false};
+  
+  play[0] = true;
+  // for(uint8_t i = 0; i < NUM_SENSORS; i++ ){
+  //   keys[i] = cs_2[i].capacitiveSensor(3);
+  //   pressed_old[i] = pressed[i];
+  //   pressed[i] = (keys[i] > PRESS_THRESHOLD)?true:false;
+  //   noInterrupts();
+  //   if (pressed[i] && !pressed_old[i]){
+  //     play[i] = true;
+  //     playCnt[i] = 0;
+  //   }
+  //   interrupts();
+  //   // Serial.print(play[i]);
+  //   // Serial.print('\t');
+  // }
+  // Serial.println();
+  if (overflow){
+    uint16_t sumTone = 0;
+
+    for(uint16_t j = 0; j < NUM_SENSORS; j++){
+      if(play[j]){
+        if(playCnt[j] < 2400){
+          playCnt[j]++; //sumTone +=
+           sumTone += pgm_read_byte_near(adresses[j] + playCnt[j]);//pgm_read_byte_near(adresses[j] + playCnt[j]);
+        } else {
+          play[j] = false;
+          playCnt[j] = 0;
+        }
+      } else {
+        playCnt[j] = 0;
+      }
+      // Serial.print(play[j]);
+      // Serial.print(':');
+      // Serial.print(playCnt[j]);
+      // Serial.print('\t');
+    }
+    // Serial.println();
+    
+
+    //map sumtone to a smaller  variable
+    OCR2A = (uint8_t) map(sumTone, 0, 255*NUM_SENSORS, 0, 255); //how many keys can you press at once? maybe this is too optimistic
+
+    // OCR2A = pgm_read_byte_near(a1_wav + i);
+    // if(i == a1_wav_len - 1){
+    //   i = 0;
+    //   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    // } 
+    // else i++;
+
+
+    overflow = false;
+    // digitalWrite(LED_BUILTIN, LOW); 
   }
-  Serial.println();
+
+
   // space check
   
   
