13d4a5c16cb38b5e7ac7eb6c2f21508a9008b724
Erik van Raalte <e.vanraalte@student.utwente.nl>
2018-11-26 21:12:19 +0100

    Did pin assignments

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,29 @@
 #include <math.h>
 
 
-// CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2); 
-// CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);
+// #define VERBOSE
 
 const size_t NUM_SENSORS = 12;
 CapacitiveSensor cs_2[NUM_SENSORS] = {
-  CapacitiveSensor(4,5),
-  CapacitiveSensor(4,6),
-  CapacitiveSensor(4,7),
-  CapacitiveSensor(4,3),
-  CapacitiveSensor(4,2),
-  CapacitiveSensor(4,3),
-  CapacitiveSensor(4,2),
-  CapacitiveSensor(4,3),
-  CapacitiveSensor(4,2),
-  CapacitiveSensor(4,3),
-  CapacitiveSensor(4,2),
-  CapacitiveSensor(4,3),
+  CapacitiveSensor(2,3),
+  CapacitiveSensor(2,4),
+  CapacitiveSensor(2,5),
+  CapacitiveSensor(2,6),
+  CapacitiveSensor(2,7),
+  CapacitiveSensor(2,8),
+  CapacitiveSensor(2,9),
+  CapacitiveSensor(2,10),
+  CapacitiveSensor(18,14),
+  CapacitiveSensor(18,15),
+  CapacitiveSensor(18,16),
+  CapacitiveSensor(18,17),
 };
+
+
 
 #define PRESS_THRESHOLD 20
 
-// #define SHORTEST_NOTE 2000
+#define SHORTEST_NOTE 2000
 
 #define SPEAKER 11
 #define DELAY  (1*1000000)/8000
@@ -40,6 +41,7 @@ volatile bool play[NUM_SENSORS] = {false};
 volatile uint16_t playCnt[NUM_SENSORS] = {0}; 
 
 const uint8_t* adresses[] = {c1_wav,c1s_wav,d1_wav,d1s_wav,e1_wav,f1_wav,f1s_wav,g1_wav,g1s_wav,a1_wav,a1s_wav,b1_wav}; //check if this works..
+const unsigned int lenghts[] = {c1_wav_len,c1s_wav_len,d1_wav_len,d1s_wav_len,e1_wav_len,f1_wav_len,f1s_wav_len,g1_wav_len,g1s_wav_len,a1_wav_len,a1s_wav_len,b1_wav_len};
 
 SIGNAL(TIMER2_OVF_vect){
   overflowCnt++;
@@ -50,8 +52,18 @@ SIGNAL(TIMER2_OVF_vect){
   }
 }
 
+void debug(char *s){
+  #ifdef VERBOSE
+  Serial.print(s);
+  #endif
+}
+
 void setup() {
+  #ifdef VERBOSE
   Serial.begin(9600);
+  #endif
+
+
   for(uint8_t i = 0; i < NUM_SENSORS; i++ ){
     cs_2[i].set_CS_AutocaL_Millis(0xFFFFFFFF); 
   }
@@ -59,7 +71,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SPEAKER, OUTPUT);
 
-  // Timer2 -- 62.5 kHz PWM
+  // Timer2 -- 62.5 kHz PWM and 7.81 kHz sample update
   TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(CS20);
   TIMSK2 |= _BV(TOIE2);
@@ -69,7 +81,6 @@ void setup() {
 
 }
 
-// the loop function runs over and over again forever
 void loop() {
   long keys[NUM_SENSORS] = {  0 }; 
   static bool pressed[NUM_SENSORS] = { false};
@@ -95,7 +106,7 @@ void loop() {
 
     for(uint16_t j = 0; j < NUM_SENSORS; j++){
       if(play[j]){
-        if(playCnt[j] < 2400){
+        if(playCnt[j] < lenghts[j] ){
           playCnt[j]++; //sumTone +=
            sumTone += pgm_read_byte_near(adresses[j] + playCnt[j]);//pgm_read_byte_near(adresses[j] + playCnt[j]);
         } else {
@@ -105,70 +116,11 @@ void loop() {
       } else {
         playCnt[j] = 0;
       }
-      // Serial.print(play[j]);
-      // Serial.print(':');
-      // Serial.print(playCnt[j]);
-      // Serial.print('\t');
-    }
-    // Serial.println();
-    
+    }   
 
     //map sumtone to a smaller  variable
-    OCR2A = (uint8_t) map(sumTone, 0, 255*NUM_SENSORS, 0, 255); //how many keys can you press at once? maybe this is too optimistic
-
-    // OCR2A = pgm_read_byte_near(a1_wav + i);
-    // if(i == a1_wav_len - 1){
-    //   i = 0;
-    //   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    // } 
-    // else i++;
-
+    OCR2A = (uint8_t) map(sumTone, 0, 255*NUM_SENSORS, 0, 255); //how many keys can you press at once? maybe this is too pessimistic
 
     overflow = false;
-    // digitalWrite(LED_BUILTIN, LOW); 
   }
-
-
-  // space check
-  
-  
-
-  // for(unsigned int i = 0; i < a1_wav_len; i ++  ){
-  //   OCR2A = 127; // pgm_read_byte_near(a1_wav + i);
-  //   delayMicroseconds(DELAY);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(a1s_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(b1_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(c1_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(c1s_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(d1_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(d1s_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(e1_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(f1_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(f1s_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(g1_wav + i);
-  // }
-  // for(unsigned int i = 0; i < SHORTEST_NOTE; i ++  ){
-  //   OCR2A = pgm_read_byte_near(g1s_wav + i);
-  // }
-
 }
